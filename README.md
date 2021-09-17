258497fe6546a9d83f46c8c51181b1abbaa2102f
Aastha Grover <aastha.grover@uwaterloo.ca>
2021-09-17 21:30:45 +0530

    Added class Admin

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # MIT_Booking_System
 Making a MIT Room booking system, employing the concepts of OOPS
+
+Here is an overview of the classes we have used to implement an efficient room booking system for Student Projects, Technical Clubs and Non-Technical clubs. 
 ![image](https://user-images.githubusercontent.com/66693577/133459860-6245f6af-d909-4e72-97dc-7e60fda63902.png)
 
 
--- club.cpp
+++ club.cpp
@@ -1,21 +1,30 @@
 #include<iostream>
-//#include<bits/stdc++>
+#include <cmath>
+#include<bits/stdc++.h>
+#include <unordered_map>
 using namespace std;
 
 #define N 30
 #define FLOOR 4
 #define ROOM 4
 static int rooms[FLOOR][ROOM]={0,0,0,0};
-#define xyz 50 
+int extra=0;
+
+unordered_map<char, string> m= {
+{'a',"Meeting"},
+{'b',"Presentation"},
+{'c',"Laboratory Work"},
+{'d',"Auditorium"}
+};
+
 class Club {
-    //protected:
-    public:
+    protected:
         char type_of_job;
         int no_of_members;
-    
+    public:
         void getType() {
-            cout<<"\nEnter type of job: ";
-            cout<<"a)Meeting \nb)Presentation \nc)Computer stuff \nd)Auditorium \n";
+            cout<<"\nEnter type of work: ";
+            cout<<"\na)Meeting \nb)Presentation \nc)Laboratory Work \nd)Auditorium \n";
             cin>>type_of_job;
         }
 
@@ -25,11 +34,15 @@ class Club {
         }
 
         void dispMemCount() {
-            cout<<endl<<no_of_members<<endl;
+            cout<<"Number of Members = "<<no_of_members<<endl;
+        }
+        
+        int retMemCount() {
+            return no_of_members;
         }
 
         void dispJobType() {
-            cout<<endl<<type_of_job<<endl;
+            cout<<"\nThe type of work you want the room for is: "<<m[type_of_job]<<endl;
         }
         
         char retJobType(){
@@ -39,90 +52,145 @@ class Club {
 
 //Array of objects created for all the below types of clubs so the records can be stored.
 class StudentProject : public Club {
-public:
-    friend int return_floor(StudentProject test);
+    private:
+        int buffer;
+    public:
+        friend int return_floor(StudentProject test);
+        void getBuffer() {
+            cout<<"Please enter the quantity of equipment ";
+            cin>>buffer;
+        }
+        void x_space() {
+            extra=ceil(buffer*1.5);
+
+            no_of_members+=extra;
+        }
 }a[N];
 
 class TechClub : public Club {
-public:
-    friend int return_floor(TechClub test);
+    private:
+        float cgpa;
+    public:
+        friend int return_floor(TechClub test);
+        void getCGPA() {
+            cout<<"Please enter the cumulative CGPA of the attendees of the room: ";
+            cin>>cgpa;
+        }
+        bool CGPA() {
+            if(cgpa>10 || cgpa<6) return 0;
+            else return 1;
+        }
 }b[N];
 
 class NonTechClub : public Club {
-public:
-    friend int return_floor(NonTechClub test);
+    private:
+        int funds;
+    public:
+        friend int return_floor(NonTechClub test);
+        void getFunds() {
+            cout<<"Please enter the funds of the Non-Technical Club: ";
+            cin>>funds;
+        }
+        bool enough_funds(int head_cnt) {
+            if(funds/head_cnt>9) return 1;
+            else return 0;
+        }
+        
+        
 }c[N];
-
 
 class Academic_block {
     protected:
-        int flr=-1;
+        int flr;
+        friend class Admin;
     public:
-        void display_rooms();
         int return_floor(StudentProject test);
         int return_floor(TechClub test);
         int return_floor(NonTechClub test);
-        int seat_count();
-        int assign_room();
+        //int seat_count();
+        int assign_room(int cnt);
+        void display_rooms();
     
 }d[N*ROOM];
 
 void Academic_block::display_rooms() {
-    cout<<"Current Status of the rooms in the Academic Block are : "<<endl;
-    cout<<"+---------------------------+"<<endl;
+    cout<<"\nCurrent Status of the rooms & Seat Count used in the Academic Block is : "<<endl;
+    cout<<"          +-----+-----+-----+-----+";
     for(int i=0;i<FLOOR;i++) {
-        cout<<"| ";
+        cout<<endl<<"Floor "<<i<<" : | ";
         for(int j=0;j<ROOM;j++) {
-            cout<<" "<<rooms[i][j]<<"| ";
+            if(rooms[i][j]>=10) cout<<" "<<rooms[i][j]<<" | ";
+            else cout<<" "<<rooms[i][j]<<"  | ";
         }
-        cout<<endl<<"+---------------------------+";
+        cout<<"\n          +-----+-----+-----+-----+";
     }
-    cout<<"+---------------------------+";
 }
+
 int Academic_block:: return_floor(StudentProject test) {
-    //cout<<"\nwelcome to floor "<< test.type_of_job;
-    if(test.type_of_job == 'a') return 0;
-    else if(test.type_of_job == 'b') return 1;
-    else if(test.type_of_job == 'c') return 2;
-    else if(test.type_of_job == 'd') return 3;
+    if(test.retJobType() == 'a') { flr=0; return 0; }
+    else if(test.retJobType() == 'b') { flr=1; return 1; }
+    else if(test.retJobType() == 'c') { flr=2; return 2; }
+    else if(test.retJobType() == 'd') { flr=3; return 3; }
     else return -1; //default case
     
 }
 
 int Academic_block:: return_floor(TechClub test) {
-    if(test.type_of_job == 'a') return 0;
-    else if(test.type_of_job == 'b') return 1;
-    else if(test.type_of_job == 'c') return 2;
-    else if(test.type_of_job == 'd') return 3;
+    if(test.retJobType() == 'a') { flr=0;return 0; }
+    else if(test.retJobType() == 'b') { flr=1; return 1; }
+    else if(test.retJobType() == 'c') { flr=2; return 2; }
+    else if(test.retJobType() == 'd') { flr=3; return 3; }
     else return -1; //default case
     
 }
 
 int Academic_block:: return_floor(NonTechClub test) {
-    //cout<<"\nwelcome to floor "<< test.type_of_job;
-    if(test.type_of_job == 'a') return 0;
-    else if(test.type_of_job == 'b') return 1;
-    else if(test.type_of_job == 'c') return 2;
-    else if(test.type_of_job == 'd') return 3;
+    if(test.retJobType() == 'a') { flr=0; return 0; }
+    else if(test.retJobType() == 'b') { flr=1; return 1; }
+    else if(test.retJobType() == 'c') { flr=2; return 2; }
+    else if(test.retJobType() == 'd') { flr=3; return 3; }
     else return -1; //default case
-    
 }
-/*int Academic_block:: return_floor(Club test) {
-    cout<<"\nwelcome to floor "<< test.type_of_job;
-    if(test.type_of_job == 'a') return 0;
-    else if(test.type_of_job == 'b') return 1;
-    else if(test.type_of_job == 'c') return 2;
-    else if(test.type_of_job == 'd') return 3;
-    else return -1; //default case
-}*/
+class Admin {
+    public:
+        void Denial(Academic_block *deny) {
+            if((*deny).flr==-1) cout<<"\nADMIN ERROR: Room type selected does not exist. Please try again later or change your requirements.\n";
+            else cout<<"\nADMIN ERROR: All compatible rooms occupied. Please try again later or change your requirements.\n";
+        }
+        
+        void cgpa_req() {
+            cout<<"\nADMIN ERROR: Sorry, CGPA requirement not fulfilled.";
+        }
+        
+        void funds_req() {
+            cout<<"\nADMIN ERROR: Sorry, Funds requirement not fulfilled.";
+        }
+};
+
+
+int Academic_block:: assign_room(int cnt) {
+    if(flr==-1) {Admin prof;
+                 prof.Denial(this);
+                 return -1;
+                } //call Admin somehow
+    if(cnt<=15 && rooms[flr][0]==0) { rooms[flr][0]=cnt-extra; return (flr+1)*100+1; }
+    else if (cnt<=25 && rooms[flr][1]==0) { rooms[flr][1]=cnt-extra; return (flr+1)*100+2; }
+    else if (cnt<=35 && rooms[flr][2]==0) { rooms[flr][2]=cnt-extra; return (flr+1)*100+3; }
+    else if (cnt<=45 && rooms[flr][3]==0) { rooms[flr][3]=cnt-extra; return (flr+1)*100+4; }
+    else  {
+        Admin prof;
+        prof.Denial(this);
+        return -1;
+    } //call Admin somehow
+}
 int main()
 {   int SP,TC,NTC,AB;//Kepps track of the respective number of organizations.
-    int choice,sol;
+    int choice,sol,r;
     SP=TC=NTC=AB=0;
     cout<<"Please select a type of organization:\n";
-    cout<<"1.Student Project \n2.Tech Club \n3.Non-Tech Club \n4.Exit  \n";
+    cout<<"1.Student Project \n2.Tech Club \n3.Non-Tech Club \n4.Display Academic Block rooms status \nOr press any key to exit. \n";
     cin>>choice;
-    bool flag;
+    bool flag=0,check;
     while(1) {
     switch (choice) {
         case 1:
@@ -131,10 +199,17 @@ int main()
             a[SP].memberCount();
             a[SP].dispJobType();
             a[SP].dispMemCount();
-           
+            a[SP].getBuffer();
+            a[SP].x_space();
+            
             sol=d[AB].return_floor(a[SP]);
-            //int sol = d[AB].return_floor(a[SP]);
-            cout<<"\nFloor: "<< sol;
+            if(sol!=-1){
+                cout<<"\nFloor: "<< sol;
+                r=d[AB].assign_room(a[SP].retMemCount());
+                if(r!=-1) cout<<"\nRoom Allotted: "<< r <<endl;
+            }
+            else cout<<"Please choose one of the given choice of work.\n";
+            //d[AB].display_rooms();
             SP++; AB++;
             break;
         case 2:
@@ -143,10 +218,22 @@ int main()
             b[TC].memberCount();
             b[TC].dispJobType();
             b[TC].dispMemCount();
-            
-            sol=d[AB].return_floor(b[TC]);
-            //int sol = d[AB].return_floor(a[SP]);
-            cout<<"\nFloor: "<< sol;
+            b[TC].getCGPA();
+            check=b[TC].CGPA();
+            if(check) {
+                sol=d[AB].return_floor(b[TC]);
+                if(sol!=-1) {
+                    cout<<"\nFloor: "<< sol;
+                    r=d[AB].assign_room(b[TC].retMemCount());
+                    if(r!=-1) cout<<"\nRoom Allotted: "<< r <<endl;
+                }
+                else cout<<"Please choose one of the given choice of work.\n";
+            }
+            else {
+                Admin logi;
+                logi.cgpa_req();
+            }
+            //d[AB].display_rooms();
             TC++; AB++;
             break;
         case 3:
@@ -155,21 +242,35 @@ int main()
             c[NTC].memberCount();
             c[NTC].dispJobType();
             c[NTC].dispMemCount();
+            c[NTC].getFunds();
+            check=c[NTC].enough_funds(c[NTC].retMemCount());
             
-            sol=d[AB].return_floor(c[NTC]);
-            //int sol = d[AB].return_floor(a[SP]);
-            cout<<"\nFloor: "<< sol;
+            if(check) {
+                sol=d[AB].return_floor(c[NTC]);
+                if(sol!=-1) {
+                    cout<<"\nFloor: "<< sol;
+                    r=d[AB].assign_room(c[NTC].retMemCount());
+                    if(r!=-1) cout<<"\nRoom Allotted: "<< r <<endl;
+                }
+                else cout<<"Please choose one of the given choice of work.\n";
+            }
+            else {
+                Admin logi;
+                logi.funds_req();
+            }
+            //d[AB].display_rooms();
             NTC++; AB++;
             break;
         case 4:
-            flag=1;
+            d[AB].display_rooms();
             break;
         default:
             flag=1;
             break;
     }
     if(flag) break;
-    cout<<"\n1.Student Project\n2.Tech Club\n3.Non-Tech Club\n4.Exit\n";
+    cout<<"\nPlease select a type of organization: ";
+    cout<<"\n1.Student Project\n2.Tech Club\n3.Non-Tech Club \n4.Display Academic Block rooms status \nOr press any key to exit. \n";
     cin>>choice;
    }
     
